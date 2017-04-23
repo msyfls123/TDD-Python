84cd3496dc5cbe066e60cc9ddb26e2ae30c8280c
msyfls123 <m18771058712@live.com>
2017-04-23 20:57:29 +0800

    Frone page HTML generated from template

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -10,3 +13,9 @@ class Homepage(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found =  resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
