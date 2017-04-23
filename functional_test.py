8eed7a4d6922907bde389fcc9fa368275f704ef9
msyfls123 <m18771058712@live.com>
2017-04-23 21:30:58 +0800

    One Step for Test

--- functional_test.py
+++ functional_test.py
@@ -29,6 +29,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(10)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
