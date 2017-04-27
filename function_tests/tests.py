9e883ccbd05a53540d2ccd417a10a8f222674ec0
msyfls123 <m18771058712@live.com>
2017-04-27 15:46:11 +0800

    new URl to display lists

--- function_tests/tests.py
+++ function_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -16,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # HomePage Title and H1 show 'To-Do lists'
         self.assertIn('To-Do lists', self.browser.title)
@@ -33,6 +34,8 @@ class NewVisitorTest(unittest.TestCase):
         # Input & Submit will get item inserted in table
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
@@ -44,6 +47,23 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table(u'1: Buy peacock feathers')
         self.check_for_row_in_list_table(u'2: Use peacock feathers to make a fly')
 
+        # A new visitor
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
 
-if __name__ == '__main__':
-    unittest.main()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
--- lists/views.py
+++ lists/views.py
@@ -11,9 +11,10 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text', '')
         Item.objects.create(text=new_item_text)
-        return redirect('/')
-    else:
-        new_item_text = ''
-    return render(request, 'home.html', {
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
+
+def view_list(request):
+    return render(request, 'list.html', {
         'items': Item.objects.all(),
     })
